c61a51cdb4a234f4aca3a623600fb3a00a6ac536
Tcheutchoua Steve <tcheutchouasteve@gmail.com>
2023-07-05 20:27:32 +0100

    root project structure with first task

---
+++ src/main/java/com/kitendo/Starter.java
@@ -0,0 +1,7 @@
+package com.kitendo;
+
+public class Starter {
+  public static void main(String[] args) {
+    System.out.println("Hello Tcheutchoua!");
+  }
+}
---
+++ src/main/java/com/kitendo/generalities/PersonName.java
@@ -0,0 +1,9 @@
+package com.kitendo.generalities;
+
+public class PersonName {
+
+  public static void main(String[] args) {
+    // change this to print your name
+    System.out.println("Hello Tcheutchoua!");
+  }
+}
