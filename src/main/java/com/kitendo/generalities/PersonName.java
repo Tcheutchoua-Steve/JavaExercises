8388fb1345866d9e24c32ae387476f7b48fe4e3b
Tcheutchoua Steve <tcheutchouasteve@gmail.com>
2023-07-06 07:53:09 +0100

    modified comment, adding Arnaud

--- src/main/java/com/kitendo/generalities/PersonName.java
+++ src/main/java/com/kitendo/generalities/PersonName.java
@@ -3,7 +3,7 @@ package com.kitendo.generalities;
 public class PersonName {
 
   public static void main(String[] args) {
-    // change this to print your name
+    // change this to print your name Arnaud
     System.out.println("Hello Tcheutchoua!");
   }
 }
